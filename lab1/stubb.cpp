f5fb937ecbe4dddced92026681f9f4d103f20067
isafo <isabelle.forsman@gmial.com>
2015-11-11 14:26:51 +0100

    ligths working need to fix animation on one light

--- lab1/stubb.cpp
+++ lab1/stubb.cpp
@@ -15,12 +15,45 @@
 #include <osg/PositionAttitudeTransform>
 #include <osg/Light>
 #include <osg/LightSource>
+#include <osg/AnimationPath>
+#include <osg/MatrixTransform>
 
 // *************** function declaration *********************************
 osg::Light* createLight(int uniqueLightnr, osg::Vec4 color);
 osg::Material *setMaterial(osg::Vec4 color);
 void update(osg::PositionAttitudeTransform* lightTransform);
 // **********************************************************************
+
+// Derive a class from NodeCallback to manipulate a
+//   MatrixTransform object's matrix.
+class RotateCB : public osg::NodeCallback
+{
+public:
+    RotateCB() : _angle( 0. ) {}
+
+    virtual void operator()( osg::Node* node,
+            osg::NodeVisitor* nv )
+    {
+        // Normally, check to make sure we have an update
+        //   visitor, not necessary in this simple example.
+        osg::MatrixTransform* mtLeft =
+                dynamic_cast<osg::MatrixTransform*>( node );
+        osg::Matrix mR, mT;
+        mT.makeTranslate( -6., 0., 0. );
+        mR.makeRotate( _angle, osg::Vec3( 0., 0., 1. ) );
+        mtLeft->setMatrix( mR * mT );
+
+        // Increment the angle for the next from.
+        _angle += 0.01;
+
+        // Continue traversing so that OSG can process
+        //   any other nodes with callbacks.
+        traverse( node, nv );
+    }
+
+protected:
+    double _angle;
+};
 
 int main(int argc, char *argv[]){
   
@@ -54,8 +87,8 @@ int main(int argc, char *argv[]){
   /// ---
 #endif
 
-  const int dimX = 256;
-  const int dimY = 256;
+  const float dimX = 6;
+  const float dimY = 6;
   
   // Add your stuff to the root node here...
   
@@ -63,13 +96,13 @@ int main(int argc, char *argv[]){
   // create ground
   osg::HeightField* ground = new osg::HeightField();
   ground->allocate(dimX, dimY);
-  ground->setXInterval(10.0f); //TODO vad gör setXinterval????? spec när man inte lopar över detta område
-  ground->setYInterval(10.0f);
+  ground->setXInterval(1.0f); //TODO vad gör setXinterval????? spec när man inte lopar över detta område
+  ground->setYInterval(1.0f);
   ground->setOrigin(osg::Vec3(0.0f, 0.0f, 0.0f));
   
   for(int i = 0; i < dimX; i++) {
     for (int j = 0; j < dimY; j++) {
-      ground->setHeight(i, j, cos(i)*9.0f + sin(j)*5.0f);
+      ground->setHeight(i, j, cos(i) + sin(j));
     }
   }
   
@@ -84,35 +117,19 @@ int main(int argc, char *argv[]){
   // read first objects
   osg::Node* gliderNode = osgDB::readNodeFile("glider.osg");
   osg::PositionAttitudeTransform* gliderTrans = new osg::PositionAttitudeTransform();
-  gliderTrans->setPosition(osg::Vec3(0.0f, 10.0f, 0.0f));
-  gliderTrans->setScale(osg::Vec3(10.0f, 10.0f, 10.0f));
+  gliderTrans->setPosition(osg::Vec3(0.0f, 0.0f, 2.0f));
   
   gliderTrans->addChild(gliderNode);
   root->addChild(gliderTrans);
 
+  // read secound objects
+  osg::Node* cessenaNode = osgDB::readNodeFile("cessna.osg");
+  osg::PositionAttitudeTransform* cessenaTrans = new osg::PositionAttitudeTransform();
+  cessenaTrans->setPosition(osg::Vec3(0.0f, 0.0f, 6.0f));
+  cessenaTrans->setScale(osg::Vec3(0.07f, 0.07f, 0.07f));
   
-  
-  /*
-  // add the ground to the scene
-  osg::Geode* gliderGeode = new osg::Geode();
-  
-  gliderGeode->addDrawable(new osg::ShapeDrawable(gliderNode));
-  
-  root->addChild(groundGeode);
-  
-  
-  // read second objects
-  osg::Node* spaceNode = osgDB::readNodeFile("spaceship.osg");
-  gliderNode->setOrigin(0.0f, 0.0f, 160.0f);
-  
-  // read third objects
-  osg::Node* cNode = osgDB::readNodeFile("cessna.osg");
-  gliderNode->setOrigin(0.0f, 0.0f, 160.0f);
-  
-  // read fourth objects
-  osg::Node* dumpNode = osgDB::readNodeFile("dumptruck.osg");
-  gliderNode->setOrigin(0.0f, 0.0f, 160.0f);*/
-  
+  cessenaTrans->addChild(cessenaNode);
+  root->addChild(cessenaTrans);
   
   // add the lights \____________________________________________________________________
   const int NR_OF_LIGHTS = 3;
@@ -123,29 +140,40 @@ int main(int argc, char *argv[]){
   
   osg::Vec4 lightColor[] = {osg::Vec4(0.0, 0.0, 0.8, 1.0), osg::Vec4(0.0, 0.8, 0.0, 1.0),  osg::Vec4(0.8, 0.0, 0.0, 1.0)};
 
-
+  osg::ref_ptr<osg::StateSet> lightSS (root->getOrCreateStateSet());
+  
   for(uniqueLightnr = 0; uniqueLightnr < NR_OF_LIGHTS; uniqueLightnr++) {
     
       // create sphere to represent the light
       lightMarker[uniqueLightnr] = new osg::Geode();
-      lightMarker[uniqueLightnr]->addDrawable(new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(0.0f, 0.0f, 0.0f), 50.0f)));
+      lightMarker[uniqueLightnr]->addDrawable(new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(0.0f, 0.0f, 0.0f), 0.05f)));
       lightMarker[uniqueLightnr]->getOrCreateStateSet()->setAttribute(setMaterial(lightColor[uniqueLightnr]));
     
       // create the light
       lightSource[uniqueLightnr] = new osg::LightSource();
       lightSource[uniqueLightnr]->setLight(createLight(uniqueLightnr, lightColor[uniqueLightnr]));
       lightSource[uniqueLightnr]->setLocalStateSetModes(osg::StateAttribute::ON);
+      lightSource[uniqueLightnr]->setStateSetModes(*lightSS,osg::StateAttribute::ON);
       
       // light transform
       lightTransform[uniqueLightnr] = new osg::PositionAttitudeTransform();
+      lightTransform[uniqueLightnr]->setPosition(osg::Vec3(2 * uniqueLightnr, 2 * uniqueLightnr, 4.0f));
+      
+      // add light marker and lightsource to transform
       lightTransform[uniqueLightnr]->addChild(lightSource[uniqueLightnr]);
       lightTransform[uniqueLightnr]->addChild(lightMarker[uniqueLightnr]);
-      lightTransform[uniqueLightnr]->setPosition(osg::Vec3(2560.0 * uniqueLightnr/3, 2560.0f * uniqueLightnr/3, 1250.0f));
       
+      if(uniqueLightnr == 2 )
+      {
+	//lightTransform[uniqueLightnr]->setUpdateCallback( new RotateCB );
+      }
+      
+      // add transform to root
       root->addChild(lightTransform[uniqueLightnr]);
   }
+ 
   
-  
+
   // not our stuff \______________________________________________________________________
   // Optimizes the scene-graph
   osgUtil::Optimizer optimizer;
@@ -158,12 +186,13 @@ int main(int argc, char *argv[]){
   return viewer.run();
 }
 
+
 void update(osg::PositionAttitudeTransform* lightTransform) {
   
   osg::ElapsedTime* timer = new osg::ElapsedTime();
   
-  lightTransform->setPosition(osg::Vec3(2560.0 * cos(timer->elapsedTime_m() /1000.0f),
-					2560.0 * sin(timer->elapsedTime_m() /1000.0f), 1250.0f));
+  lightTransform->setPosition(osg::Vec3(0.1 * cos(timer->elapsedTime_m() /10.0f),
+					0.1 * sin(timer->elapsedTime_m() /10.0f), 1.0f));
   
   
 }
